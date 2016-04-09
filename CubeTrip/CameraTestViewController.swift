9bebf10215fc0d0779cb1af4a4daea1dd9259b50
Manjit Bedi <dev@manjitbedi.com>
2016-04-08 17:11:36 -0700

    Experiment 4
    just getting started
    
    TODO:
    
    add a ground plane
    add some objects
    set up some way points
    animate camera between the way points

---
+++ CubeTrip/CameraTestViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CameraTestViewController.swift
+//  CubeTrip
+//
+//  Created by Manjit Bedi on 2016-04-08.
+//  Copyright © 2016 coldharbourmedia. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class CameraTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new scene
+//        let scene = SCNScene(named: "Spheres.scn")!
+        let scene = SCNScene()
+        
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        
+        // configure the view
+        scnView.backgroundColor = UIColor.blackColor()
+        
+        scene.background.contents = ["Skybox_right1", "Skybox_left2", "Skybox_top3", "Skybox_bottom4", "Skybox_front5", "Skybox_back6"];
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
