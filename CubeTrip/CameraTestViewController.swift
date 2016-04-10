774717566685dcaae0034452a0ddb5f2d365954f
Manjit Bedi <dev@manjitbedi.com>
2016-04-09 17:26:44 -0700

    Loading a tree into the scene
    it took some doing to first get a tree & then load it into the scene.

--- CubeTrip/CameraTestViewController.swift
+++ CubeTrip/CameraTestViewController.swift
@@ -8,37 +8,60 @@
 
 import UIKit
 import SceneKit
+import SceneKit.ModelIO
 
 class CameraTestViewController: UIViewController {
 
+    var scene: SCNScene?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // create a new scene
-        let scene = SCNScene(named: "CameraAnimation.scn")!
+        
+        if let scene = SCNScene(named: "CameraAnimation.scn") {
+            self.scene = scene
 
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.blackColor()
-        
-        scene.background.contents = ["Skybox_right1", "Skybox_left2", "Skybox_top3", "Skybox_bottom4", "Skybox_front5", "Skybox_back6"];
+            // retrieve the SCNView
+            let scnView = self.view as! SCNView
+            
+            // set the scene to the view
+            scnView.scene = self.scene
+            
+            // allows the user to manipulate the camera
+            scnView.allowsCameraControl = true
+            
+            // show statistics such as fps and timing information
+            scnView.showsStatistics = true
+            
+            self.addTrees()
+            
+            // configure the view
+            scnView.backgroundColor = UIColor.blackColor()
+            
+            self.scene!.background.contents = ["Skybox_right1", "Skybox_left2", "Skybox_top3", "Skybox_bottom4", "Skybox_front5", "Skybox_back6"];
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+     This is interesting,  you can load a obj files from scene kit asset catalog.
+     Does this mean the object has been convrted to a SceneKit scene?
+     There is only one object in the scene so it is safe to jut get the
+     first objects attached to the root node.
+     
+     TODO: create nodes at different postions and attach a tree to each node.
+     
+    */
+    func addTrees() {
+        if let treeScene = SCNScene(named: "nature.scnassets/Tree_01.obj") {
+            print(treeScene)
+            self.scene?.rootNode.addChildNode(treeScene.rootNode.childNodes.first!)
+        }
     }
     
 
